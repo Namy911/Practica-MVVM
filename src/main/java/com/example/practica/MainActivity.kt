748bba0a3844a9c7322c6122805fd3a087a5f2e7
Namy911 <hodorogeaandrei@gmail.com>
2020-09-28 12:24:49 +0300

    spinner: select category v1

--- src/main/java/com/example/practica/MainActivity.kt
+++ src/main/java/com/example/practica/MainActivity.kt
@@ -1,18 +1,10 @@
 package com.example.practica
 
-import android.app.AlertDialog
-import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.ArrayAdapter
-import android.widget.ListView
-import androidx.fragment.app.DialogFragment
-import com.example.practica.ui.main.EditArticleFragment
 import com.example.practica.ui.main.MainFragment
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.android.synthetic.main.list_dialog.*
-import kotlinx.android.synthetic.main.list_row_edit.*
+
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
--- src/main/java/com/example/practica/ui/main/EditArticleFragment.kt
+++ src/main/java/com/example/practica/ui/main/EditArticleFragment.kt
@@ -1,48 +1,29 @@
 package com.example.practica.ui.main
 
-import android.app.Activity
-import android.app.Activity.RESULT_OK
-import android.app.AlertDialog
-import android.app.Dialog
 import android.content.Context
-import android.content.DialogInterface
-import android.content.Intent
-import android.graphics.Bitmap
 import android.net.Uri
 import android.os.Bundle
-import android.provider.MediaStore
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
-import android.widget.ListView
-import android.widget.RadioButton
-import androidx.activity.result.contract.ActivityResultContract
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.core.os.bundleOf
 import androidx.core.view.get
-import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MediatorLiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.Observer
-import androidx.recyclerview.widget.ListAdapter
 import com.example.practica.R
 import com.example.practica.data.entity.Article
 import com.example.practica.data.entity.Category
 import com.example.practica.databinding.ListRowEditBinding
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.android.synthetic.main.list_row_edit.*
-import kotlinx.coroutines.flow.asFlow
-import kotlinx.coroutines.flow.emptyFlow
+import kotlinx.android.synthetic.main.sppiner_row_category.view.*
 
 
 @AndroidEntryPoint
-class EditArticleFragment : Fragment() {
+class EditArticleFragment : Fragment(), AdapterView.OnItemSelectedListener {
     companion object {
         private const val TAG = "EditArticleFragment"
         private const val TAG_DIALOG = "CategoryDialog"
@@ -54,30 +35,53 @@ class EditArticleFragment : Fragment() {
     }
 
     private var articleId: Int = -1
+    private lateinit var category: Category
     private lateinit var binding: ListRowEditBinding
     private val viewModel: MainViewModel by viewModels()
 
-        override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?) =
+        override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+        ) =
         ListRowEditBinding.inflate(layoutInflater, container, false)
         .apply { binding = this }
         .root
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        // Set article data from view: name, title, desc, image
         if(binding.model == null) {
             articleId = requireArguments().getInt(ARTICLE_ID, -1)
             viewModel.loadArticleToEdit(articleId)
             viewModel.articleToEdit.observe(viewLifecycleOwner, { binding.model = it })
         }
 
-        val getContent = registerForActivityResult(ActivityResultContracts.GetContent()) { uri: Uri? -> binding.imgSelect.setImageURI(uri) }
+        val getContent = registerForActivityResult(ActivityResultContracts.GetContent()) { uri: Uri? -> binding.imgSelect.setImageURI(
+            uri
+        ) }
         binding.btnImage.setOnClickListener { getContent.launch("image/*") }
 
-
+        // Set spinner with all categories
+        viewModel.categories.observe(viewLifecycleOwner){
+//            val spinnerAdapter = ArrayAdapter(
+//                requireContext(),
+//                android.R.layout.simple_spinner_dropdown_item,
+//                it.map { it.name })
+//            binding.spinCategory.adapter = spinnerAdapter
+            binding.spinCategory.adapter = SpinnerCategoryAdapter(requireContext(), it)
+        }
+        binding.spinCategory.onItemSelectedListener = this
+        
         binding.bottNav?.setOnNavigationItemSelectedListener { item ->
             when(item.itemId){
-                R.id.bot_menu_save -> { viewModel.saveArticle(getArticle()); true }
-                R.id.bot_menu_save_new -> { CategoryDialog().show(childFragmentManager, TAG_DIALOG) ; true }
+                R.id.bot_menu_save -> {
+                    viewModel.saveArticle(getArticle()); true
+                }
+                R.id.bot_menu_save_new -> {
+                    Log.d(TAG, "onViewCreated: ${getArticle()}"); true
+                }
                 else -> { false }
             }
         }
@@ -88,33 +92,34 @@ class EditArticleFragment : Fragment() {
             binding.edtDesc.text.toString(),
             binding.edtContent.text.toString(),
             1,
-            1
+            category.id
         )
 
-    @AndroidEntryPoint
-    class CategoryDialog: DialogFragment(){
-        companion object{
-            const val LIST_CATEGORIES = "ui.main.list.categories"
-            fun newInstance(list: List<Category>) = CategoryDialog().apply {
-                arguments = bundleOf(LIST_CATEGORIES to list)
-            }
-        }
-
-        override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-            val view: View = layoutInflater.inflate(R.layout.list_dialog, null)
-
-            return activity?.let {
-                val builder = AlertDialog.Builder(it)
-                builder.setMessage("Salut")
-                    .setMultiChoiceItems(R.array.dialog2, null,
-                DialogInterface.OnMultiChoiceClickListener { dialog, which, isChecked ->
-                })
-//                    .setView(view)
-                    .setPositiveButton("Da", {_,_ ->})
-                builder.create()
-            }?: throw IllegalStateException("Activity cannot be null")
-        }
-
+    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+        category = parent?.getItemAtPosition(position) as Category
+        Log.d(TAG, "onItemSelected: ${category} ")
     }
-    
+    override fun onNothingSelected(parent: AdapterView<*>?) {
+        TODO("Not yet implemented")
+    }
+    // Create list of all categories in spinner
+    inner class SpinnerCategoryAdapter(context: Context, items: List<Category>):
+        ArrayAdapter<Category>(context, 0, items) {
+
+        override fun getDropDownView(position: Int, convertView: View?, parent: ViewGroup): View {
+            return this.createView(position, convertView, parent)
+        }
+
+        override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+            return this.createView(position, convertView, parent)
+        }
+        // Create view and associate data
+        private fun createView(position: Int, recycledView: View?, parent: ViewGroup): View {
+            val category = getItem(position)
+            val view = recycledView ?:
+                        LayoutInflater.from(context).inflate(R.layout.sppiner_row_category, parent, false)
+            view.txt_category.text = category!!.name
+            return view
+        }
+    }
 }
